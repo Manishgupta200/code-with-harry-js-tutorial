3a1ccdf4422bf4e7634d0f321662cf3c40ca8daf
manish20079 <guptamanis27@gmail.com>
2021-03-21 10:34:35 +0530

    (4) DOM get method

--- myscript.js
+++ myscript.js
@@ -1,3 +1,18 @@
-// DOM(document object module)
-var element=document.all[6];
-document.write(element);
+// DOM get methods
+
+// get innertext
+var element;
+element=document.getElementById('menu').innerText;
+// innerHTML
+element=document.getElementById('menu').innerHTML;
+// grtAttribute, getAttributeNode
+element=document.getElementById('header')
+.getAttributeNode('style').name;
+// attributes
+element=document.getElementById('header').attributes;
+
+console.log(element);
+function xyz(){
+	console.warn('it"s dangerous');
+}
+// .name & .value works in getAttributeNode, attributes
