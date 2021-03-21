bad4d37f2f10d221d1ca770d7aed099cb37d54c7
manish20079 <guptamanis27@gmail.com>
2021-03-21 06:41:25 +0530

    (3) js multiple function create

--- myscript.js
+++ myscript.js
@@ -1,1 +1,10 @@
-console.log('hello000');
+function sum(math,eng,sci){
+	var s=math+eng+sci;
+	return s;
+}
+function percentage(tt){
+	var p=tt/300*100;
+	document.write(p);
+}
+var total=sum(2,3,7);
+percentage(total);
