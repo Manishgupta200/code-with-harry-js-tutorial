1e95c829ed2b2b265ed9f3aa528dd0b2b1fd72d1
manish20079 <guptamanis27@gmail.com>
2021-03-21 07:11:53 +0530

    js events

--- myscript.js
+++ myscript.js
@@ -1,10 +1,1 @@
-function sum(math,eng,sci){
-	var s=math+eng+sci;
-	return s;
-}
-function percentage(tt){
-	var p=tt/300*100;
-	document.write(p);
-}
-var total=sum(2,3,7);
-percentage(total);
+// EVENTS
