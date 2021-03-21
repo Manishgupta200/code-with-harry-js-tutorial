a8fe0b0dee8c3bbb5056d10959ab7032e5ac598d
manish20079 <guptamanis27@gmail.com>
2021-03-21 21:11:51 +0530

    (5) DOM set method

--- myscript.js
+++ myscript.js
@@ -1,18 +1,35 @@
-// DOM get methods
-
-// get innertext
+// DOM set method
 var element;
-element=document.getElementById('menu').innerText;
-// innerHTML
-element=document.getElementById('menu').innerHTML;
-// grtAttribute, getAttributeNode
+// innerText
 element=document.getElementById('header')
-.getAttributeNode('style').name;
-// attributes
-element=document.getElementById('header').attributes;
+.innerText='<h2>in text tag not works</h2>';
+// innerHTML
+element=document.getElementById('header')
+.innerHTML='<h1>overwrited div</h1>i am visible';
+// setAttribute --in #header class abc replace to pqr
+document.getElementById('header')
+.setAttribute('class','pqr');
+// get after set attribute then work console.log
+element=document.getElementById('header')
+.getAttributeNode('class');
+// more priority then these style inside HTML
+document.getElementById('header')
+.setAttribute('style',"border: 1px dotted blue");
 
+element=document.getElementById('header')
+.getAttributeNode('style');
+// attributeof header of 1 index now  replace to xyz
+document.getElementById('header')
+.attributes[1].value='xyz';
+
+element=document.getElementById('header').attributes[1];
+// sort form direct set it to var elemet
+element=document.getElementById('header')
+.attributes[1].value="zxc";
+// removeAttribute now #header's class removed from all 
+document.getElementById('header')
+.removeAttribute('class');
+// show null in console because class name removed
+element=document.getElementById('header')
+.getAttributeNode('class');
 console.log(element);
-function xyz(){
-	console.warn('it"s dangerous');
-}
-// .name & .value works in getAttributeNode, attributes
