a78b5c790400d40fc71d41e45ec21541ae2b7189
manish20079 <guptamanis27@gmail.com>
2021-03-22 12:45:25 +0530

    (7) DOM css styling method, className, classList, addEventListener method

--- myscript.js
+++ myscript.js
@@ -1,27 +1,80 @@
-// DOM query selector (css selector)
+// DOM css styling method
 var element;
-// 1st class of list select if many avilable
-element=document.querySelector('.list');
-// taking all list class in array form
-element=document.querySelectorAll('.list');
-// take index 1 of array of class list 
-element=document.querySelectorAll('.list')[1];
-// show only text without inside tag 
-// of index 1 of array of class list
-element=document.querySelectorAll('.list')[1].innerText;
-// also work innerHTML
-element=document.querySelectorAll('.list')[1].innerHTML;
-// show in this type --<ul class="list">…</ul>
-element=document.querySelector('ul');
-// NodeList [ul.list] --show in this type
-// 0: ul.list
-// length: 1
-element=document.querySelectorAll('ul');
-// NodeList(2) [h2, h2] --show like these
-// 0: h2
-// 1: h2
+// style of #header change from blue to tan
+ // dosn't effect of border-radius
+ document.querySelector('#header')
+ .style.backgroundColor='tan';
+// get method of style
+element=document.querySelector('#header').style;
+// return border-radius value 100px
+element=document.querySelector('#header')
+.style.borderRadius;
+// DOM class name
+// set class and removed xyz class
+document.querySelector('#content').className="abc";
+// return like this-- abc
+element=document.querySelector('#content').className;
+// set two class symeltaneously and removed xyz class
+// we can style from .abc or .pqr
+document.querySelector('#content').className='abc pqr';
+// return like this(get method)-- abc pqr
+element=document.querySelector('#content').className;
+// DOM class list
+// add multiple class and return in array form
+document.querySelector('#content').classList='abc pqr xyz';
+// shown like these--
+// DOMTokenList(3) ["abc", "pqr", "xyz", value:"abc pqr xyz"]
+// 0: "abc"
+// 1: "pqr"
+// 2: "xyz"
+// length: 3
+// value: "abc pqr xyz"
+element=document.querySelector('#content').classList;
+// add extra class in #content show like these--
+// 0: "abc"
+// 1: "pqr"
+// 2: "xyz"
+// 3: "man"
+// length: 4
+// value: "abc pqr xyz man"
+document.querySelector('#content').classList.add('man');
+// abc & xyz class now removed from div of #content show--
+// DOMTokenList(2) ["pqr", "man", value: "pqr man"]
+// 0: "pqr"
+// 1: "man"
 // length: 2
-element=document.querySelectorAll('#content h2');
-// <h2>i'm 2nd heading2</h2> --show h2 of index 1
-element=document.querySelectorAll('#content h2')[1];
+// value: "pqr man"
+document.querySelector('#content')
+.classList.remove('abc','xyz');
+// DOM addEventListener method
+// onclick on div of header2 bg-color shown red
+document.getElementById('header2').onclick=abc;
+// aso use-- document.querySelector('#header2').onclick=abc;
+function abc(){
+document.getElementById('header2')
+.style.backgroundColor='red';
+}
+// mouseenter in div of id header2 function
+ // sass run andcolor shown blue and border style not removed
+ // If take abc function then above abc function shown
+  // and color shown red
+document.getElementById('header2')
+.addEventListener('mouseenter',sass);
+function sass(){
+	document.querySelector('#header2')
+	.style.backgroundColor='blue';
+}
+// direct insert a function in addEvent listener matod
+// when mouseenter color shows blue & when enter here 
+// border shows 5px dotted #84C9B1 and color changes to red
+document.querySelector('#header2')
+.addEventListener('click',function(){
+	document.getElementById('header2')
+	.style.border='5px dotted #84C9B1';
+});
+// use of-- this (very sorthand property)
+// click on div of id header color chhange tan to red
+document.querySelector('#header').addEventListener('click',function(){
+	this.style.backgroundColor='red';
+});
 console.log(element);
