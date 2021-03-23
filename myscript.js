8f5930ff387e02164eeb5d7e273058bcb3ba417b
manish20079 <guptamanis27@gmail.com>
2021-03-23 16:58:56 +0530

    (12) DOM appent method; appendChild

--- myscript.js
+++ myscript.js
@@ -1,15 +1,17 @@
-// DOM create method
-// with the hel of javascript create tag,text,comment inside HTML file
-// <h2></h2> --return
-// here h2 tag are creates not insertd in html
+// DOM append method
+// h2 tag created
 var newElement=document.createElement('h2');
-// here  text are creates not insertd in html
-// "this is text node" --return
-var newText=document.createTextNode('this is text node');
-// here  comment are creates not insertd in html
-// <!--this is comment--> --return
-var newComment=document.createComment('this is comment');
-
+// text created
+var newText=document.createTextNode('i am text created in js');
+// DOM appendChild
+// means created text are now inserted in created h2 tag
+newElement.appendChild(newText);
+// <h2>i am text created in js</h2> --return
 console.log(newElement);
+// "i am text created in js" --return
 console.log(newText);
-console.log(newComment);
+// (i am text created in js) --return on bouwser page
+// newElement means (<h2>i am text created in js</h2>) are now inserted in div of #text
+// appendChild attach things in last  of any writing
+document.getElementById('text').appendChild(newElement);
+// DOM insertBefore
