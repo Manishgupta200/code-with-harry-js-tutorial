b5bcf43a16eaded5198459625baf53ac37a5485f
manish20079 <guptamanis27@gmail.com>
2021-03-23 18:51:21 +0530

    (14) DOM insertAdjacent method; insertAdjacentElement, insertAdjacentHTML, insertAdjacentText

--- myscript.js
+++ myscript.js
@@ -1,14 +1,29 @@
-// DOM insertBefore
-var newElement=document.createElement('h2');
+// DOM insertAdjacent method
+// create and append symeltaneously
+// DOM insertAdjacent position for append 
+// beforebegin
+// afterbegin
+// beforeend
+// afterend
+// DOM insertAdjacentElement --only tag
+var newElement=document.createElement('h1');
 var newText=document.createTextNode('i am text created in js');
-newElement.appendChild(newText);
-// firstly #text are takingthrough var target
+// "i am text created in js" is now inserted in created h1
+newElement.append(newText);
+// targeting div of #text
 var target=document.getElementById('text');
-// and then insertBefore things where i want (i insert newElement (<h2>i am text created in js</h2>) in childNodes[0] pace)
-target.insertBefore(newElement,target.childNodes[0]);
+// <h1>i am text created in js</h1> is now inserted in div of #text in beforebegin position
+target.insertAdjacentElement('beforebegin',newElement);
+// DOM insertAdjacentHTML --tag with text
+// targeting div of #text2
+var target2=document.getElementById('text2');
+var newElement2='<h1>i am created in var newElement2</h1>';
+// <h1>i am text created in js</h1> is now inserted in div of #text2 in afterend position
+target2.insertAdjacentHTML('afterend',newElement2);
+// DOM insertAdjacentText --only text
+// targeting div of #text3
+var target3=document.getElementById('text3');
+var newElement3='i am created in var new Element3';
+// 'i am text created in js' is now inserted in div of #text3 in afterbegin position
+target3.insertAdjacentText("afterend",newElement3);
 
-
-// <h2>i am text created in js</h2> --return
-console.log(newElement);
-// i am text created in js --return
-console.log(newText);
