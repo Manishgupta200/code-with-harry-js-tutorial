9e9236631c03db9dc42ba9a5a2d301975f3d8a62
manish20079 <guptamanis27@gmail.com>
2021-03-23 20:23:12 +0530

    (17) DOM cloneNode method

--- myscript.js
+++ myscript.js
@@ -1,5 +1,12 @@
-// DOM removeChild
-var target=document.querySelector('#list');
-var list1=target.children[0];
+// DOM cloneNode method
+var target=document.getElementById('list-a').children[0];
+// it copy <li></li> from #lisa-a
+// if nothing say true and false then default value it false which copies only tags and attributes not inner text
+var copyElement=target.cloneNode();
 
-target.removeChild(list1);
+// it copy <li>list-a1</li> from #lisa-a
+var copyElement=target.cloneNode(true);
+// <li>list-a1</li>
+console.log(copyElement);
+
+document.getElementById('list-b').appendChild(copyElement);
