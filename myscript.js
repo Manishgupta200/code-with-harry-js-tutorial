c0ddd439319df527dcbe110b6c30500f35806d83
manish20079 <guptamanis27@gmail.com>
2021-03-24 19:17:32 +0530

    (21) animation property: setInterval()

--- myscript.js
+++ myscript.js
@@ -1,1 +1,19 @@
-// JS form basic event
+// animation property
+// setInterval()
+var a=0;
+// after every 1000ms/(1s) of interval anime fn. run which says console.log(a) run continuously
+// 10 --return
+// 20
+// 30
+// .
+// .
+// . 
+setInterval(anime,1000);
+
+function anime(){
+	a=a+10;
+	console.log(a);
+
+	var target=document.getElementById('test');
+	target.style.marginLeft=a+'px';
+}
