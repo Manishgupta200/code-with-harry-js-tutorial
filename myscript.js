3eee697232270396874807e68331cd2b7d3c05e5
manish20079 <guptamanis27@gmail.com>
2021-03-24 13:47:01 +0530

    (20) JS form basic event; onfocus, onblur, oninput, onchange, onselect, onsubmit, oninvalid

--- myscript.js
+++ myscript.js
@@ -1,10 +1,1 @@
-// DOM isEqualNode()
-var target1=document.getElementById('list1').firstElementChild;
-var target2=document.getElementById('list2').children[1];
-// true --return
-// actually it compare two things if match then return true else false
-var comp=target1.isEqualNode(target2);
-
-console.log(target1);
-console.log(target2);
-console.log(comp);
+// JS form basic event
