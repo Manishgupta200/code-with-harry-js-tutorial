2bb8fee2d359eed137b4f24d581d76ae4f5bfbab
manish20079 <guptamanis27@gmail.com>
2021-03-27 20:52:03 +0530

    (23) animation property; clearInterval by 2nd metod example

--- myscript.js
+++ myscript.js
@@ -1,23 +1,15 @@
 // animation property
-// setInterval()
-var a=0;
-// after every 1000ms/(1s) of interval anime fn. run which says console.log(a) run continuously
-// 10 --return
-// 20
-// 30
-// .
-// .
-// . 
-var id=setInterval(anime,1000);
-function anime(){
-
-	a=a+10;
-	var target=document.getElementById('test');
-	target.style.marginLeft=a+'px';
-	console.log(a);
 // clearInterval()
-// if a=100 reach the after it setInterval() is cear
+var a=0;
+var set=setInterval(anime,1000);
+function anime(){
+	a=a+10;
+	console.log(a);
 	if(a==100){
-		clearInterval(id);
+		clearInterval(set);
+	}
+	else{
+		var target=document.getElementById('test');
+		target.style.marginLeft=a+'px';
 	}
 }
