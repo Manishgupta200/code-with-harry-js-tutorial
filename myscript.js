d98321d27b11786057a27452cbf7769b50a3c571
manish20079 <guptamanis27@gmail.com>
2021-03-29 18:13:56 +0530

    (26) BOM window.open();, .close();

--- myscript.js
+++ myscript.js
@@ -1,13 +1,22 @@
 // BOM (browser object module)
-// window height and width method
-function resizeFn(){
-	console.clear();
-// when resize browser height then in console iHeight and oHeight shows and 
-// because of console.clear() previous is cleared continuously
-	var iHeight=window.innerHeight;
-	console.log('Inner Height: ' + iHeight);
-
-	var oHeight=window.outerHeight;
-	console.log('Outer Height: ' + oHeight);
+// window open
+// window.open('URL','(eg:- _blank,_self,_parent,_top) or any name','width=100px,height,left,top');
+function openWindow(){
+// blank new window open of same browser window on other tab
+	// window.open();
+// amazon site open in new window of same browser in anothet tab
+	// window.open('https://www.amazon.com');
+// new window open of amazon in in new browser window in top left position of screen with width=500px and height 200px
+	// window.open('https://www.amazon.com','','width=500px,height=200px');
+// new window open of amazon in in new browser window in top=100px left=200px position of screen with width=500px and height 200px	
+	// window.open('https://www.amazon.com','hello','width=500px,height=300px,top=100px,left=200px');
 }
-// same for width
+
+// window close
+function openWindow(){
+	open=window.open('https://www.amazon.com','hello','width=500px,height=300px,top=100px,left=200px');
+}
+function closeWindow(){
+// opened window is now closed and after it again click on openWindow button then not open new window
+	open.close();
+}
