c65effbb298a898b36499dad94f2e5b3f5438f31
manish20079 <guptamanis27@gmail.com>
2021-03-30 15:10:59 +0530

    (40) modify and remove array element

--- myscript.js
+++ myscript.js
@@ -1,33 +1,10 @@
-// multidimensional array
-var ary=[
-	[1,2,3,4],
-	[5,6,7,8],
-	[9,10,11,12],
-	[13,14,15,16]
-];
-document.write(ary.length);
-document.write('<table border="1px" cellspacing="0">');
-for(var a=0;a<ary.length;a++){
-	document.write('<tr>');
-	for(var b=0;b<ary[a].length;b++){
-		document.write('<td>',ary[a][b],'</td>');
-	}
-	document.write('</tr>');
-}
-document.write('</table>');
-
-
-
-
-for(var a=0;a<ary.length;a++){
-	document.write(ary[a] + '<br>');
-}
-
-
-
-for(var a=0;a<ary.length;a++){
-	for(var b=0;b<ary[a].length;b++){
-		document.write(ary[a][b]+' ');
-	}
-	document.write('<br>');
-}
+// modify and remove array element
+var a=['harry',18,'male',true];
+document.write(a,'<br>');
+a[0]='sunny';
+document.write(a,'<br>');
+delete a[1];
+document.write(a,'<br>');
+document.write(a[1],'<br>');
+a[6]='hello';
+document.write(a);
