1c49160257f4b9bb744825367c1f8ad888a2d768
manish20079 <guptamanis27@gmail.com>
2021-03-30 14:12:19 +0530

    (36) array method

--- myscript.js
+++ myscript.js
@@ -1,11 +1,10 @@
-// history object method
-console.log(history);
-console.log(history.length);
-// goes backward
-history.back();
-// goes forward
-history.forward();
-// goes one page forward
-history.go(1);
-// goes two page backward
-history.go(-2);
+// array
+var sum=0;
+var ary=[10,20,30,40,50,10];
+document.write("<ul style='font-weight:bold'>");
+for(var a=0;a<ary.length;a++){
+	document.write("<li>",ary[a],'</li>');
+	sum=sum+ary[a];
+}
+document.write('</ul>');
+document.write("SUM is: ",sum);
