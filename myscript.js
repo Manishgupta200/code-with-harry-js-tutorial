bcf31971fb5c468a59483e26005765a66273e782
manish20079 <guptamanis27@gmail.com>
2021-03-30 20:45:10 +0530

    (48) const with array and object

--- myscript.js
+++ myscript.js
@@ -1,17 +1,9 @@
-// array of object
-var student=[
-	{name:'ram',age:15},
-	{name:'karan',age:13},
-	{name:'rahish',age:9}
-];
-// 0: {name: "ram", age: 15} --return
-// 1: {name: "karan", age: 13}
-// 2: {name: "rahish", age: 9}
-// length: 3
-console.log(student);
-// [object Object],[object Object],[object Object] --return
-document.write(student,'<br>');
-
-for(var a=0;a<student.length;a++){
-	document.write(student[a].name,'<br>');
-}
+// const with array and object
+const a=[10,20,30];
+document.write(a,'<br>');
+// a=30;
+// error occur by a=30;
+// console.log(a);
+a[1]=25;
+document.write(a);
+// same for object
