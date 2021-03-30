a34ec005f7b77588aa3cce8d244a00908931e77b
manish20079 <guptamanis27@gmail.com>
2021-03-30 10:49:33 +0530

    (34) location object method (set)

--- myscript.js
+++ myscript.js
@@ -1,10 +1,15 @@
 // window location object method
-// get method
-console.log(location);
-console.log(location.host);
-console.log(location.hostname);
-console.log(location.href);
-console.log(location.port);
-console.log(location.protocol);
-console.log(location.hash);
-console.log(location.search);
+// set method
+function reloadFn(){
+// when click on reloadFn btn then page refresh
+	location.reload();
+}
+function assignFn(){
+// when click on assignFn btn then google page open and previous page not deleted, when i want go backward
+	location.assign('https://www.google.com');
+}
+
+function replaceFn(){
+// when click on replaceFn btn then github page open and previous page now deleted and not able to go backward
+	location.replace('https://www.github.com');
+}
