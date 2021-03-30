9eb6c2cc7f6c563d9f3ed22b386038dcde73576d
manish20079 <guptamanis27@gmail.com>
2021-03-30 20:40:51 +0530

    (47) array of object

--- myscript.js
+++ myscript.js
@@ -1,11 +1,17 @@
-// object new method
-var obj=new Object();
-obj.fname='manish';
-obj.lname='gupta';
-obj.age=25;
-// [object Object] --return
-document.write(obj,'<br>');
-// 25 --return
-document.write(obj.age,'<br>');
-// 25 --return
-document.write(obj['age']);
+// array of object
+var student=[
+	{name:'ram',age:15},
+	{name:'karan',age:13},
+	{name:'rahish',age:9}
+];
+// 0: {name: "ram", age: 15} --return
+// 1: {name: "karan", age: 13}
+// 2: {name: "rahish", age: 9}
+// length: 3
+console.log(student);
+// [object Object],[object Object],[object Object] --return
+document.write(student,'<br>');
+
+for(var a=0;a<student.length;a++){
+	document.write(student[a].name,'<br>');
+}
