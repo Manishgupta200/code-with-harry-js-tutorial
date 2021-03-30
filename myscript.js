b169af0e8bae5f9189131c9972901e50d3026fa0
manish20079 <guptamanis27@gmail.com>
2021-03-30 21:22:52 +0530

    (49) for/in loop

--- myscript.js
+++ myscript.js
@@ -1,9 +1,24 @@
-// const with array and object
-const a=[10,20,30];
-document.write(a,'<br>');
-// a=30;
-// error occur by a=30;
-// console.log(a);
-a[1]=25;
-document.write(a);
-// same for object
+// for/in loop
+var obj={
+	fname:'yahoo',
+	lname:'baba',
+	age:25,
+	email:'java@23.com'
+};
+// yahoo --return
+// baba
+// 25
+// java@23.com
+for(var i in obj){
+	document.write(obj[i],'<br>');
+}
+
+document.write('<br>');
+
+// fname --return
+// lname
+// age
+// email
+for(var key in obj){
+	document.write(key,'<br>');
+}
