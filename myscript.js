9659cacdc68f20473efe25f92d8abc1025b67160
manish20079 <guptamanis27@gmail.com>
2021-03-30 11:06:13 +0530

    (35) history object method

--- myscript.js
+++ myscript.js
@@ -1,15 +1,11 @@
-// window location object method
-// set method
-function reloadFn(){
-// when click on reloadFn btn then page refresh
-	location.reload();
-}
-function assignFn(){
-// when click on assignFn btn then google page open and previous page not deleted, when i want go backward
-	location.assign('https://www.google.com');
-}
-
-function replaceFn(){
-// when click on replaceFn btn then github page open and previous page now deleted and not able to go backward
-	location.replace('https://www.github.com');
-}
+// history object method
+console.log(history);
+console.log(history.length);
+// goes backward
+history.back();
+// goes forward
+history.forward();
+// goes one page forward
+history.go(1);
+// goes two page backward
+history.go(-2);
