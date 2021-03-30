ca52e3d23dde8d2b05c3dc0eba3a6afe9b5fd26f
manish20079 <guptamanis27@gmail.com>
2021-03-30 09:48:33 +0530

    (30)  BOM resizeBy method

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
 // BOM (browser object module)
-// resizeTo()
+// resizeBy()
 var myWindow;
 function openWindow(){
 	myWindow=window.open('','','width=500px,height=300px');
 }
 function resizeWindow(){
-// window heght and width ate change to width=400px & height=400px
-	myWindow.resizeTo(400,400);
+// same as resizeTo() method width and height are added relative to previous new window size
+	myWindow.resizeBy(400,400);
 }
