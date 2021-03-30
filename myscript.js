5ff334907e0b61219ec541fee2f2c4340767ccc1
manish20079 <guptamanis27@gmail.com>
2021-03-30 15:29:31 +0530

    (41) array method; sort, reverse, pop, push, shift, unshift

--- myscript.js
+++ myscript.js
@@ -1,10 +1,21 @@
-// modify and remove array element
-var a=['harry',18,'male',true];
+// array method
+var a=['Sanjay','Aman','Ram','Karan'];
 document.write(a,'<br>');
-a[0]='sunny';
+// print in alphabetical order
+a.sort();
 document.write(a,'<br>');
-delete a[1];
+// print in reverse order
+a.reverse();
 document.write(a,'<br>');
-document.write(a[1],'<br>');
-a[6]='hello';
-document.write(a);
+// delete last value in array
+a.pop();
+document.write(a,'<br>');
+// add extra value in last position
+a.push('Rahul');
+document.write(a,'<br>');
+// delete first value in array
+a.shift();
+document.write(a,'<br>');
+// add extra value in first position
+a.unshift('Manish');
+document.write(a,'<br><br>');
