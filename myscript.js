cad3ae632add3cfc225a77dfac3a9831dbdbd56e
manish20079 <guptamanis27@gmail.com>
2021-03-30 20:32:05 +0530

    (46) object new method

--- myscript.js
+++ myscript.js
@@ -1,43 +1,11 @@
-
-// OBJECT
-var a={
-	fname:'manish',
-	lname:'gupta',
-	age:19,
-	email:'guptamanis27@gmail.com',
-	ary:['1st','2nd','3rd'],
-	fn:function(){
-		return 2500;
-	},
-	fullname:function(){
-		// for callinf the fname and lname property this. is used
-		return this.fname+' '+this.lname;
-	},
-	innerobj:{
-		'city':'raniganj',
-		country:'india'
-	}
-}
+// object new method
+var obj=new Object();
+obj.fname='manish';
+obj.lname='gupta';
+obj.age=25;
 // [object Object] --return
-document.write(a,'<br>');
-// age: 25 --return
-// email: "email"
-// fname: "fname"
-// lname: "lname"
-console.log(a);
-// manish --return
-console.log(a.fname);
-// manish --return
-document.write(a.fname,'<br>');
-// 1st,2nd,3rd --return
-document.write(a.ary,'<br>');
-// 1st --return
-document.write(a.ary[0],'<br>');
-// 2500 --return
-document.write(a.fn(),'<br>');
-// manish gupta --return
-document.write(a.fullname(),'<br>');
-// raniganj --return
-document.write(a.innerobj.city,'<br>');
-// india --return
-document.write(a.innerobj.country);
+document.write(obj,'<br>');
+// 25 --return
+document.write(obj.age,'<br>');
+// 25 --return
+document.write(obj['age']);
