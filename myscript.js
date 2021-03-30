eaceb68219560378900e00215cac12610dfcd831
manish20079 <guptamanis27@gmail.com>
2021-03-30 10:01:27 +0530

    (31) BOM scrollBy method

--- myscript.js
+++ myscript.js
@@ -1,10 +1,8 @@
 // BOM (browser object module)
-// resizeBy()
-var myWindow;
-function openWindow(){
-	myWindow=window.open('','','width=500px,height=300px');
-}
-function resizeWindow(){
-// same as resizeTo() method width and height are added relative to previous new window size
-	myWindow.resizeBy(400,400);
+// scrollBy()
+function scrollWindow(){
+// x axis horizontalscroll(left -ve, right +ve) & y axis vertical scroll(above -ve, below +ve)
+// here x=0,y=20
+// click on scrollWindow btn again and again then 20 px scroll down symeltaneousy
+	window.scrollBy(0,20);
 }
