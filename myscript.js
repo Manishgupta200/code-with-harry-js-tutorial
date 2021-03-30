28deb13e187de714d2913c235f0ece2f7a3a14fd
manish20079 <guptamanis27@gmail.com>
2021-03-30 15:03:12 +0530

    (39) multidimensional array method

--- myscript.js
+++ myscript.js
@@ -1,10 +1,33 @@
-// array new method
-var ary=new Array(3);
+// multidimensional array
+var ary=[
+	[1,2,3,4],
+	[5,6,7,8],
+	[9,10,11,12],
+	[13,14,15,16]
+];
+document.write(ary.length);
+document.write('<table border="1px" cellspacing="0">');
 for(var a=0;a<ary.length;a++){
-	ary[a]=prompt('Enter the value: ');
+	document.write('<tr>');
+	for(var b=0;b<ary[a].length;b++){
+		document.write('<td>',ary[a][b],'</td>');
+	}
+	document.write('</tr>');
 }
-document.write("<ul>");
+document.write('</table>');
+
+
+
+
 for(var a=0;a<ary.length;a++){
-	document.write('<li>',ary[a],'</li>');
+	document.write(ary[a] + '<br>');
 }
-document.write("</ul>");
+
+
+
+for(var a=0;a<ary.length;a++){
+	for(var b=0;b<ary[a].length;b++){
+		document.write(ary[a][b]+' ');
+	}
+	document.write('<br>');
+}
