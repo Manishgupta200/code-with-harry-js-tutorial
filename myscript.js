f9068cb3a00a8c9de778911f9b5b5d9751c83afb
manish20079 <guptamanis27@gmail.com>
2021-03-30 16:38:11 +0530

    (42) array method; concat, join

--- myscript.js
+++ myscript.js
@@ -1,21 +1,22 @@
 // array method
-var a=['Sanjay','Aman','Ram','Karan'];
-document.write(a,'<br>');
-// print in alphabetical order
-a.sort();
-document.write(a,'<br>');
-// print in reverse order
-a.reverse();
-document.write(a,'<br>');
-// delete last value in array
-a.pop();
-document.write(a,'<br>');
-// add extra value in last position
-a.push('Rahul');
-document.write(a,'<br>');
-// delete first value in array
-a.shift();
-document.write(a,'<br>');
-// add extra value in first position
-a.unshift('Manish');
-document.write(a,'<br><br>');
+var a=['1st','2nd','3rd','4th'];
+// Sanjay,Aman,Ram,Karan --return
+document.write(a+'<br>');
+// Contat1 & Concat2 are added in last of a
+ // Sanjay,Aman,Ram,Karan,Concat1,Concat2 --return
+var b=a.concat('Concat1','Concat2');
+document.write(b+'<br><br>');
+// Sanjay,Aman,Ram,Karan,Sanjay,Aman,Ram,Karan,Concat1,Concat2 --return
+var c=a.concat(b);
+document.write(c+'<br>');
+// Sanjay,Aman,Ram,Karan,Concat1,Concat2,Sanjay,Aman,Ram,Karan --return
+var c=b.concat(a);
+document.write(c+'<br><br>');
+var d=['d1','d2'];
+// concat a then b then d
+// 1st,2nd,3rd,4th,1st,2nd,3rd,4th,Concat1,Concat2,d1,d2 --return
+var c=a.concat(b,d);
+document.write(c+'<br><br>');
+// it makes a string value by joining all array value
+var j=a.join('-');
+document.write(j);
