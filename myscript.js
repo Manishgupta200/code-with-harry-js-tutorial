541a561cf113c6d791492fc47905cb48c4287886
manish20079 <guptamanis27@gmail.com>
2021-03-30 09:08:48 +0530

    (27) DOM moveTo method

--- myscript.js
+++ myscript.js
@@ -1,22 +1,15 @@
 // BOM (browser object module)
-// window open
-// window.open('URL','(eg:- _blank,_self,_parent,_top) or any name','width=100px,height,left,top');
+// moveTo()
+var myWindow;
 function openWindow(){
-// blank new window open of same browser window on other tab
-	// window.open();
-// amazon site open in new window of same browser in anothet tab
-	// window.open('https://www.amazon.com');
-// new window open of amazon in in new browser window in top left position of screen with width=500px and height 200px
-	// window.open('https://www.amazon.com','','width=500px,height=200px');
-// new window open of amazon in in new browser window in top=100px left=200px position of screen with width=500px and height 200px	
-	// window.open('https://www.amazon.com','hello','width=500px,height=300px,top=100px,left=200px');
+	myWindow=window.open('https://www.amazon.com','hello','height=500px,width=700px,left=100px,top=100px');
+	myWindow.document.write('<p>this is my paragraph</p>');
+}
+function moveWindow(){
+// move mindow 200px from top and 200 px from left
+	myWindow.moveTo(200,200);
+// focus on new window after move in through 1st window
+	myWindow.focus();
 }
 
-// window close
-function openWindow(){
-	open=window.open('https://www.amazon.com','hello','width=500px,height=300px,top=100px,left=200px');
-}
-function closeWindow(){
-// opened window is now closed and after it again click on openWindow button then not open new window
-	open.close();
-}
+
