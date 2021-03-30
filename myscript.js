abeb60a8919862762e948c784d866c659f893b0d
manish20079 <guptamanis27@gmail.com>
2021-03-30 10:31:51 +0530

    (33) location object method (get)

--- myscript.js
+++ myscript.js
@@ -1,11 +1,10 @@
-// BOM (browser object module)
-// scrollTo()
-function scrollWindow(){
-// page is in top and left position
-// this is defult position also
-	window.scrollTo(0,0);
-// on click on scrollWindow btn window are scroll down by 50px once time only
-	window.scrollTo(0,50);
-}
-// scrollTo() works one time only
-// scrollTo() takes absolute position
+// window location object method
+// get method
+console.log(location);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.href);
+console.log(location.port);
+console.log(location.protocol);
+console.log(location.hash);
+console.log(location.search);
