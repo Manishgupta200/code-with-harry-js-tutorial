15bffad17e080c6e64d240c32e02a05ffc4c2bb9
manish20079 <guptamanis27@gmail.com>
2021-03-30 22:02:02 +0530

    (50) array map method

--- myscript.js
+++ myscript.js
@@ -1,24 +1,20 @@
-// for/in loop
-var obj={
-	fname:'yahoo',
-	lname:'baba',
-	age:25,
-	email:'java@23.com'
-};
-// yahoo --return
-// baba
-// 25
-// java@23.com
-for(var i in obj){
-	document.write(obj[i],'<br>');
+// arry map() fn.
+// create a new array by calculating on all value  of array 
+var ary=[10,20,30,40];
+// 100,200,300,400 --return
+var b=ary.map(test);
+document.write(b,'<br>');
+function test(x){
+	return x*10;
 }
 
-document.write('<br>');
-
-// fname --return
-// lname
-// age
-// email
-for(var key in obj){
-	document.write(key,'<br>');
+var ary2=[
+	{fname:'manish',lname:'gupta'},
+	{fname:'manish1',lname:'gupta1'},
+	{fname:'manish2',lname:'gupta2'}
+];
+var b2=ary2.map(test2);
+document.write(b2);
+function test2(x){
+	return x.fname;
 }
